ce6ed640854aab2a68385e5ba07ba8693accc0f4
Michael Yuan <michael@secondstate.io>
2021-11-25 20:52:52 +0000

    Add nohup
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- containerd/http_server/http_server_application.sh
+++ containerd/http_server/http_server_application.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo ctr i pull docker.io/avengermojo/http_server:with-wasm-annotation
 echo -e "Creating POD ..."
-sudo -b ctr run --rm --runc-binary crun --runtime io.containerd.runc.v2 --label module.wasm.image/variant=compat docker.io/avengermojo/http_server:with-wasm-annotation http-server-example /http_server.wasm
+nohup sudo -b ctr run --rm --runc-binary crun --runtime io.containerd.runc.v2 --label module.wasm.image/variant=compat docker.io/avengermojo/http_server:with-wasm-annotation http-server-example /http_server.wasm
 echo -e "Sleeping for 10 seconds"
 sleep 10
 echo -e "Awake again"
