d8cc95e28f2efa76d44ce0fe903d95d8535fad84
Michael Yuan <michael@secondstate.io>
2021-11-25 20:29:48 +0000

    Put the server process to background
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- containerd/http_server/http_server_application.sh
+++ containerd/http_server/http_server_application.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo ctr i pull docker.io/avengermojo/http_server:with-wasm-annotation
 echo -e "Creating POD ..."
-sudo ctr run --rm --runc-binary crun --runtime io.containerd.runc.v2 --label module.wasm.image/variant=compat docker.io/avengermojo/http_server:with-wasm-annotation http-server-example /http_server.wasm
+sudo -b ctr run --rm --runc-binary crun --runtime io.containerd.runc.v2 --label module.wasm.image/variant=compat docker.io/avengermojo/http_server:with-wasm-annotation http-server-example /http_server.wasm
 echo -e "\n\nGetting containers\n\n"
 sudo ctr container ls
 sudo ctr task ls
@@ -11,5 +11,6 @@ echo -e "Awake again"
 echo -e "\n\nGetting containers\n\n"
 sudo ctr container ls
 sudo ctr task ls
+curl -d "name=WasmEdge" -X POST http://127.0.0.1:1234
 echo -e "\n\nFinished\n\n"
 echo -e "Done!"
