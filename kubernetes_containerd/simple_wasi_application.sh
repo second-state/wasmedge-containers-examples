7efa2073727d15cc5a8398afcb99b7ee619d65df
Michael Yuan <michael@secondstate.io>
2021-12-17 22:12:31 +0000

    Try to fix k8s install
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- kubernetes_containerd/simple_wasi_application.sh
+++ kubernetes_containerd/simple_wasi_application.sh
@@ -1,4 +1,11 @@
-export KUBECONFIG=/var/run/kubernetes/admin.kubeconfig
+#!/bin/bash
+export KUBERNETES_PROVIDER=local
+sudo ./kubernetes/cluster/kubectl.sh config set-cluster local --server=https://localhost:6443 --certificate-authority=/var/run/kubernetes/server-ca.crt
+sudo ./kubernetes/cluster/kubectl.sh config set-credentials myself --client-key=/var/run/kubernetes/client-admin.key --client-certificate=/var/run/kubernetes/client-admin.crt
+sudo ./kubernetes/cluster/kubectl.sh config set-context local --cluster=local --user=myself
+sudo ./kubernetes/cluster/kubectl.sh config use-context local
+sudo ./kubernetes/cluster/kubectl.sh
 
+sudo ./kubernetes/cluster/kubectl.sh cluster-info
 sudo ./kubernetes/cluster/kubectl.sh run --restart=Never wasi-demo --image=hydai/wasm-wasi-example:with-wasm-annotation --annotations="module.wasm.image/variant=compat" --overrides='{"kind":"Pod", "apiVersion":"v1", "spec": {"hostNetwork": true}}' /wasi_example_main.wasm 50000000
 
