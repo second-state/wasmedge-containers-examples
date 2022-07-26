890fa09ba529026ff6a0780a3f30ae384ea421a9
hydai <z54981220@gmail.com>
2022-07-27 06:00:00 +0800

    Refacotr install script and workflow
    Signed-off-by: hydai <z54981220@gmail.com>

--- containerd/install.sh
+++ containerd/install.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+
+export WASMEDGE_VERSION=""
+export CRUN_VERSION=""
+export ZH="FALSE"
+
+for opt in "$@"; do
+  case $opt in
+    -w=*|--wasmedge=*)
+      export WASMEDGE_VERSION="${opt#*=}"
+      shift
+      ;;
+    -c=*|--crun=*)
+      export CRUN_VERSION="${opt#*=}"
+      shift
+      ;;
+    --zh=*)
+      export ZH="${opt#*=}"
+      shift
+      ;;
+    *)
+      ;;
+  esac
+done
+
 echo -e "Starting installation ..."
 sudo apt update
 export VERSION="1.5.7"
@@ -18,6 +42,10 @@ sudo systemctl daemon-reload
 # change containerd conf to use crun as default
 sudo mkdir -p /etc/containerd/
 sudo bash -c "containerd config default > /etc/containerd/config.toml"
+if [[ "$ZH" = "TRUE" ]]; then
+    echo -e "Use ZH mirror"
+    sudo sed -i 's/k8s.gcr.io\/pause:3.5/mirrorgooglecontainers\/pause:3.5/g' /etc/containerd/config.toml
+fi
 wget https://raw.githubusercontent.com/second-state/wasmedge-containers-examples/main/containerd/containerd_config.diff
 sudo patch -d/ -p0 < containerd_config.diff
 sudo systemctl start containerd
@@ -29,20 +57,36 @@ then
 fi
 wget -q https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.sh
 sudo chmod a+x install.sh
-sudo ./install.sh --path="/usr/local"
-rm -rf install.sh
 
+if [[ "$WASMEDGE_VERSION" = "" ]]; then
+    echo -e "Use latest WasmEdge release"
+    sudo ./install.sh --path="/usr/local"
+else
+    echo -e "Use WasmEdge: $WASMEDGE_VERSION"
+    sudo ./install.sh --path="/usr/local" --version=$WASMEDGE_VERSION
+fi
+
+rm -rf install.sh
 echo -e "Building and installing crun"
 sudo apt install -y make git gcc build-essential pkgconf libtool libsystemd-dev libprotobuf-c-dev libcap-dev libseccomp-dev libyajl-dev go-md2man libtool autoconf python3 automake
 
-git clone https://github.com/containers/crun
+if [[ "$CRUN_VERSION" = "" ]]; then
+    echo -e "Use latest master of Crun"
+    git clone https://github.com/containers/crun
+else
+    echo -e "Use Crun: $CRUN_VERSION"
+    echo -e "Downloading crun-${CRUN_VERSION}.tar.gz"
+    wget https://github.com/containers/crun/releases/download/${CRUN_VERSION}/crun-${CRUN_VERSION}.tar.gz
+    tar --no-overwrite-dir -xzf crun-${CRUN_VERSION}.tar.gz
+    mv crun-${CRUN_VERSION} crun
+fi
+
 cd crun
 ./autogen.sh
 ./configure --with-wasmedge
 make
 sudo make install
 
-# change containerd conf to use crun as default
 
 sudo systemctl restart containerd
 echo -e "Finished"
--- crio/install.sh
+++ crio/install.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+
+export WASMEDGE_VERSION=""
+export CRUN_VERSION=""
+export ZH="FALSE"
+
+for opt in "$@"; do
+  case $opt in
+    -w=*|--wasmedge=*)
+      export WASMEDGE_VERSION="${opt#*=}"
+      shift
+      ;;
+    -c=*|--crun=*)
+      export CRUN_VERSION="${opt#*=}"
+      shift
+      ;;
+    --zh=*)
+      export ZH="${opt#*=}"
+      shift
+      ;;
+    *)
+      ;;
+  esac
+done
+
 echo -e "Starting installation ..."
 sudo apt update
 export OS="xUbuntu_20.04"
@@ -7,6 +31,7 @@ echo -e "OS: $OS"
 echo -e "Version: $VERSION"
 echo -e "Installing libseccomp2 ..."
 sudo apt install -y libseccomp2
+
 # Set download URL that we are adding to sources.list.d
 export ADD1="deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /"
 export DEST1="/etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
@@ -23,8 +48,8 @@ then
     echo "$ADD1" | sudo tee -i "$DEST1"
     echo "Success writing to $DEST1"
 else
-	echo "Error writing to $DEST1"
-	exit 2
+    echo "Error writing to $DEST1"
+    exit 2
 fi
 
 if [ -f "$DEST2" ]
@@ -32,8 +57,8 @@ then
     echo "$ADD2" | sudo tee -i "$DEST2"
     echo "Success writing to $DEST2"
 else
-	echo "Error writing to $DEST2"
-	exit 2
+    echo "Error writing to $DEST2"
+    exit 2
 fi
 echo -e "Installing wget"
 sudo apt install -y wget
@@ -67,20 +92,46 @@ then
 fi
 wget -q https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.sh
 sudo chmod a+x install.sh
-sudo ./install.sh --path="/usr/local"
+
+if [[ "$WASMEDGE_VERSION" = "" ]]; then
+    echo -e "Use latest WasmEdge release"
+    sudo ./install.sh --path="/usr/local"
+else
+    echo -e "Use WasmEdge: $WASMEDGE_VERSION"
+    sudo ./install.sh --path="/usr/local" --version=$WASMEDGE_VERSION
+fi
+
 rm -rf install.sh
 echo -e "Building and installing crun"
 sudo apt install -y make git gcc build-essential pkgconf libtool libsystemd-dev libprotobuf-c-dev libcap-dev libseccomp-dev libyajl-dev go-md2man libtool autoconf python3 automake
 
-git clone https://github.com/containers/crun
+if [[ "$CRUN_VERSION" = "" ]]; then
+    echo -e "Use latest master of Crun"
+    git clone https://github.com/containers/crun
+else
+    echo -e "Use Crun: $CRUN_VERSION"
+    echo -e "Downloading crun-${CRUN_VERSION}.tar.gz"
+    wget https://github.com/containers/crun/releases/download/${CRUN_VERSION}/crun-${CRUN_VERSION}.tar.gz
+    tar --no-overwrite-dir -xzf crun-${CRUN_VERSION}.tar.gz
+    mv crun-${CRUN_VERSION} crun
+fi
+
 cd crun
 ./autogen.sh
 ./configure --with-wasmedge
 make
 sudo make install
+
 # Write config
-echo -e "[crio.runtime]\ndefault_runtime = \"crun\"\n" | sudo tee -i /etc/crio/crio.conf
-echo -e "\n# Add crun runtime here\n[crio.runtime.runtimes.crun]\nruntime_path = \"/usr/local/bin/crun\"\nruntime_type = \"oci\"\nruntime_root = \"/run/crun\"\n" | sudo tee -i -a /etc/crio/crio.conf.d/01-crio-runc.conf
+if [[ "$ZH" = "FALSE" ]]; then
+    echo -e "[crio.runtime]\ndefault_runtime = \"crun\"\n" | sudo tee -i /etc/crio/crio.conf
+    echo -e "\n# Add crun runtime here\n[crio.runtime.runtimes.crun]\nruntime_path = \"/usr/local/bin/crun\"\nruntime_type = \"oci\"\nruntime_root = \"/run/crun\"\n" | sudo tee -i -a /etc/crio/crio.conf.d/01-crio-runc.conf
+else
+    echo -e "Use ZH mirror"
+    echo -e "[crio.runtime]\ndefault_runtime = \"crun\"\n[crio.image]\n
+    pause_image = \"registry.cn-hangzhou.aliyuncs.com/google-containers/pause-amd64:3.0\"\n" | sudo tee -i /etc/crio/crio.conf
+    echo -e "\n# Add crun runtime here\n[crio.runtime.runtimes.crun]\nruntime_path = \"/usr/local/bin/crun\"\nruntime_type = \"oci\"\nruntime_root = \"/run/crun\"\n" | sudo tee -i -a /etc/crio/crio.conf.d/01-crio-runc.conf
+fi
 
 sudo systemctl restart crio
 echo -e "Finished"
